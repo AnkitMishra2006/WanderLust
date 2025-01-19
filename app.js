87cc35242cee263bea37a675d47d68d2f64f01dc
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-01-20 02:46:57 +0530

    Create new Listing route done

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 main()
   .then(() => {
     console.log("Connection to DB Successful ");
@@ -26,9 +29,33 @@ app.get("/", (req, res) => {
   res.send("I am Root");
 });
 
-app.get("/listings", (req, res) => {
-  Listing.find({}).then((res) => console.log(res));
+// Index Route
+app.get("/listings", async (req, res) => {
+  const listings = await Listing.find({});
+  res.render("listings/index.ejs", { listings });
 });
+
+// New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+// Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+// Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings")
+});
+
+
+
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
 });
